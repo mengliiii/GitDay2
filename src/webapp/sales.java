7bd04869244b228a40781bdf5cca02c4865f7453
mengliiii <menglivy@hotmail.com>
2022-12-21 15:20:31 -0500

    added requirements

--- src/webapp/sales.java
+++ src/webapp/sales.java
@@ -8,5 +8,8 @@ public class sales {
     //one more change
     //more sales
 
+    //in the github, when i pulled my merger request, someone wants changes to be done with the codes
+    //i come to my local branch again, making some changes accordingly
+
 
 }
